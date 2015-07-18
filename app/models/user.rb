91683096c6678c25fa7d3c45aa194d6abdc8ba75
Jason Shultz <jasshultz@gmail.com>
2015-07-18 17:25:09 -0600

    touch ups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :confirmable, :invitable, :registerable
-  after_create :override_confirmable_email
-    
+  devise :database_authenticatable, :invitable, :registerable
+
   def override_confirmable_email
     self.confirmed_at = DateTime.current
     self.save
