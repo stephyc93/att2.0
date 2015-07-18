35212b22571db007aa9dcb84d16cb589d4b5a68f
stephyc93 <stephyc93@gmail.com>
2015-07-18 17:41:44 -0600

    added teacher controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'teachers/index'
+
   # devise_for :users, controllers: {
   #       sessions: 'users/sessions',
   #       registrations: 'users/registrations'
